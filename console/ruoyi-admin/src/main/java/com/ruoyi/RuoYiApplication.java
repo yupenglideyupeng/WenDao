4d210cad91c962f0803477f26a427c09befd4994
张毓鹏 <1667022151@qq.com>
2026-06-02 10:28:17 +0800

    fix:启动类调整

--- console/ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ console/ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -14,7 +14,6 @@ public class RuoYiApplication
 {
     public static void main(String[] args)
     {
-        // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
     }
 }
